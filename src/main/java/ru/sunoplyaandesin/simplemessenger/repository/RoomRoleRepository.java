428dad93dc9ce6fa7f0c4069bdcbfb222fb16daf
Max Sunoplya <89170567837@mail.ru>
2021-05-25 23:21:23 +0400

    Annotation @Repository added.

--- src/main/java/ru/sunoplyaandesin/simplemessenger/repository/RoomRoleRepository.java
+++ src/main/java/ru/sunoplyaandesin/simplemessenger/repository/RoomRoleRepository.java
@@ -1,7 +1,9 @@
 package ru.sunoplyaandesin.simplemessenger.repository;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 import ru.sunoplyaandesin.simplemessenger.domain.RoomRole;
 
+@Repository
 public interface RoomRoleRepository extends CrudRepository<RoomRole, Long> {
 }
