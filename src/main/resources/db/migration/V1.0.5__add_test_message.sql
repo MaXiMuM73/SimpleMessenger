eb4c41783b0af8338cbaacd3324b5bcd878d4b82
Max Sunoplya <89170567837@mail.ru>
2021-06-09 11:34:18 +0300

    Edited description.

--- src/main/resources/db/migration/V1.0.5__add_test_message.sql
+++ src/main/resources/db/migration/V1.0.5__add_test_message.sql
@@ -1,4 +1,4 @@
--- INSERT TEST USER
+-- INSERT TEST MESSAGE
 INSERT INTO public.messages (created_date, text, room_id, user_id)
 VALUES ('2021-06-08T20:44:56.745+00:00',
         'test message text',
