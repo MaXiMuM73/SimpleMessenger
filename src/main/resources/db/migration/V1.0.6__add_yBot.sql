97b2ef670bb718f81c776189a5f80bc1a4428f11
Max Sunoplya <89170567837@mail.ru>
2021-06-09 11:35:02 +0300

    Added yBot migration and youtube api dependencies.

---
+++ src/main/resources/db/migration/V1.0.6__add_yBot.sql
@@ -0,0 +1,5 @@
+-- INSERT YBOT
+INSERT INTO public.users (name, password, system_role)
+VALUES ('ybot',
+        '$2a$10$a9.lZuVu26XqQ/eLnADtTO2wfLCzduKQgYbXmDaBKKVrrteKXb7nu',
+        'SYSTEM_BOT');
