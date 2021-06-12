808827d44a6e8ea4d8a7e4740e2224d7296f32e1
Max Sunoplya <89170567837@mail.ru>
2021-06-12 18:31:53 +0400

    Added yBot room migration.

---
+++ src/main/resources/db/migration/V1.0.7__add_yBot_room.sql
@@ -0,0 +1,6 @@
+-- INSERT YBOT ROOM
+INSERT INTO public.rooms (created_date, private, title, user_id)
+VALUES ('2021-06-08T20:44:56.745+00:00',
+        'false',
+        'YOUTUBE BOT ROOM',
+        '6');
