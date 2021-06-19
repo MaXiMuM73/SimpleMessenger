a7854622e98185bddd90c0fd752e083277317a9e
Max Sunoplya <89170567837@mail.ru>
2021-06-19 13:00:15 +0400

    Fixed exception.

--- src/main/java/ru/sunoplyaandesin/simplemessenger/controller/impl/DefaultAdvice.java
+++ src/main/java/ru/sunoplyaandesin/simplemessenger/controller/impl/DefaultAdvice.java
@@ -8,13 +8,16 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import ru.sunoplyaandesin.simplemessenger.dto.response.Response;
 import ru.sunoplyaandesin.simplemessenger.exception.*;
 
+import java.net.UnknownHostException;
+
 @ControllerAdvice
 public class DefaultAdvice {
 
     @ExceptionHandler({UserNotFoundException.class,
             RoomNotFoundException.class, MessageNotFoundException.class,
-            WrongPasswordException.class, UserRoomRoleNotFoundException.class})
-    public ResponseEntity<Response> handleException(UserNotFoundException e) {
+            WrongPasswordException.class, UserRoomRoleNotFoundException.class,
+            YoutubeNotFoundException.class, UnknownHostException.class})
+    public ResponseEntity<Response> handleException(Exception e) {
         Response response = new Response(e.getMessage());
         return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
     }
