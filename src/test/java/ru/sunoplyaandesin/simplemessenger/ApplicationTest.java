f9f707cd674192428cf01e2ea6c3ffddc6d957db
Max Sunoplya <89170567837@mail.ru>
2021-06-28 00:40:09 +0400

    Added application run test.

---
+++ src/test/java/ru/sunoplyaandesin/simplemessenger/ApplicationTest.java
@@ -0,0 +1,12 @@
+package ru.sunoplyaandesin.simplemessenger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTest {
+
+    @Test
+    void contextLoads() {
+    }
+}
